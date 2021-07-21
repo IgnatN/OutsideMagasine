39400781cba5ed96e1111e934236c0c0cd7f0a2d
vasile.drozdovschi <vaseav57@gmail.com>
2021-07-21 20:59:08 +0300

    vasiles commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
-import { style } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
-import { OutsideMagasineService } from './service/outside.magasine.service';
-import { Product } from './service/type';
+
 
 @Component({
   selector: 'app-root',
@@ -9,30 +7,6 @@ import { Product } from './service/type';
   styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent implements OnInit {
-  user = {
-    name: 'Colea',
-  }
-  productChartList: number[] = [];
-  chartCount: number = 0;
-  products: Product[] = [];
-  constructor(private productService: OutsideMagasineService) {
-  }
+export class AppComponent {
 
-  ngOnInit(): void {
-    this.products = this.productService.getProducts()
-
-  }
-
-  addProduct(id: number) {
-    let box = this.productChartList.indexOf(id);
-    console.log(box)
-    if (box >= 0) {
-      this.chartCount--;
-      this.productChartList.splice(box, 1)
-      return;
-    }
-    this.chartCount++;
-    this.productChartList.push(id)
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductComponent } from './product/product.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { ProductPageComponent } from './components/product-page/product-page.component';
+import { ProductComponent } from './components/product-page/product/product.component';
+
+
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
-
+    HeaderComponent,
+    ProductPageComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
